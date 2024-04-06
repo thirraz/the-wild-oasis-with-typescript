7cacec9bbdb13838caf2d27928f6d3de7592a645
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-04-06 03:19:47 -0300

    feature: app layout

---
+++ src/ui/AppLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Sidebar from "./Sidebar"
+import Header from "./Header"
+
+export default function AppLayout() {
+	return (
+		<div className="h-[100dvh] grid [grid-template-columns:26rem_1fr] [grid-template-rows:auto_1fr]">
+			<Sidebar />
+			<Header />
+
+			<main className="bg-grey-50 p-[4rem_4.8rem_6.4rem]">
+				<Outlet />
+			</main>
+		</div>
+	)
+}
---
+++ src/ui/Sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+	return (
+		<aside className="bg-grey-0 py-[3.2rem] px-[2.4rem] border-r-[1px] border-grey-100 row-span-full">
+			Sidebar
+		</aside>
+	)
+}
