5834a7a60bf465724d73786fc71ec16566bd98e9
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-04-08 11:53:08 -0300

    update: fetching app settings

--- src/features/settings/UpdateSettingsForm.tsx
+++ src/features/settings/UpdateSettingsForm.tsx
@@ -1,24 +1,60 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form"
+import FormRow from "../../ui/FormRow"
+import Spinner from "../../ui/Spinner"
+import { useSettings } from "./useSettings"
 
 function UpdateSettingsForm() {
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
-    </Form>
-  );
+	const {
+		isPending,
+		settings: {
+			minBookingLength,
+			maxBookingLength,
+			maxGuestsPerBooking,
+			breakfastPrice
+		} = {}
+	} = useSettings()
+
+	if (isPending) return <Spinner />
+
+	return (
+		<Form>
+			<FormRow label="Minimum nights/booking">
+				<input
+					type="number"
+					id="min-nights"
+					defaultValue={minBookingLength}
+					className="input"
+				/>
+			</FormRow>
+
+			<FormRow label="Maximum nights/booking">
+				<input
+					type="number"
+					id="max-nights"
+					defaultValue={maxBookingLength}
+					className="input"
+				/>
+			</FormRow>
+
+			<FormRow label="Maximum guests/booking">
+				<input
+					type="number"
+					id="max-guests"
+					defaultValue={maxGuestsPerBooking}
+					className="input"
+				/>
+			</FormRow>
+
+			<FormRow label="Breakfast price">
+				<input
+					type="number"
+					id="breakfast-price"
+					defaultValue={breakfastPrice}
+					className="input"
+				/>
+			</FormRow>
+		</Form>
+	)
 }
 
-export default UpdateSettingsForm;
+export default UpdateSettingsForm
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,13 @@
+import UpdateSettingsForm from "../features/settings/UpdateSettingsForm"
+import Row from "../ui/Row"
+
 function Settings() {
-	return <h1 className="text-4xl">Update hotel settings</h1>
+	return (
+		<Row>
+			<h1 className="text-4xl">Update hotel settings</h1>
+			<UpdateSettingsForm />
+		</Row>
+	)
 }
 
 export default Settings
