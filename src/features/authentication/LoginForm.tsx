8975629bb17509386f1462dd553ef0cf2c952f01
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-04-15 12:08:19 -0300

    update: login form

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -1,41 +1,48 @@
-import { useState } from "react";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import Input from "../../ui/Input";
-import FormRowVertical from "../../ui/FormRowVertical";
+import { useState } from "react"
+import { Button } from "../../ui/Button"
+import Form from "../../ui/Form"
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
 
-  function handleSubmit() {}
+	function handleSubmit() {}
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <FormRowVertical label="Email address">
-        <Input
-          type="email"
-          id="email"
-          // This makes this form better for password managers
-          autoComplete="username"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormRowVertical>
-      <FormRowVertical label="Password">
-        <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormRowVertical>
-      <FormRowVertical>
-        <Button size="large">Login</Button>
-      </FormRowVertical>
-    </Form>
-  );
+	return (
+		<Form
+			onSubmit={handleSubmit}
+			className="space-y-6 bg-grey-0 px-16 py-16 rounded-md max-w-[40rem] w-[40rem] flex flex-col"
+		>
+			<div className="flex flex-col gap-2 text-lg w-full">
+				<label htmlFor="email">E-email</label>
+				<input
+					type="email"
+					id="email"
+					// This makes this form better for password managers
+					autoComplete="username"
+					value={email}
+					onChange={e => setEmail(e.target.value)}
+					className="border border-grey-300 bg-grey-0 rounded-sm py-2 px-4 shadow-sm outline-brand-500"
+				/>
+			</div>
+			<div className="flex flex-col gap-2 text-lg">
+				<label htmlFor="password">Password</label>
+				<input
+					type="password"
+					id="password"
+					autoComplete="current-password"
+					value={password}
+					onChange={e => setPassword(e.target.value)}
+					className="border border-grey-300 bg-grey-0 rounded-sm py-2 px-4 shadow-sm outline-brand-500"
+				/>
+			</div>
+			<div>
+				<Button size="large" className="w-full py-4">
+					Login
+				</Button>
+			</div>
+		</Form>
+	)
 }
 
-export default LoginForm;
+export default LoginForm
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,14 @@
+import LoginForm from "../features/authentication/LoginForm"
+import Logo from "../ui/Logo"
+
 function Login() {
 	return (
-		<main className="min-h-[100dvh] grid [grid-template-columns: 48rem] content-center justify-center gap-[3.2rem] bg-grey-50">
-			Login
+		<main className="min-h-[100dvh] flex flex-col items-center justify-center bg-grey-100 gap-5">
+			<Logo />
+			<h3 className="text-2xl uppercase font-bold mt-3">
+				Log in to your account
+			</h3>
+			<LoginForm />
 		</main>
 	)
 }
