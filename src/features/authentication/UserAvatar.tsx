823adcef79a2ecd75849dd238208e45f5e8db10e
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-04-22 10:40:11 -0300

    update: user avatar size

--- src/features/authentication/UserAvatar.tsx
+++ src/features/authentication/UserAvatar.tsx
@@ -9,7 +9,7 @@ export default function UserAvatar() {
 			<img
 				src={avatar || "/default-user.jpg"}
 				alt={`Profile picture of ${fullName}`}
-				className="block w-12 aspect-square-[1] object-cover object-center rounded-[50%] outline-1 outline-grey-100 dark:outline-dark-grey-100"
+				className="block w-[50px] h-[50px] object-cover object-center rounded-[50%] outline-1 outline-grey-100 dark:outline-dark-grey-100"
 			/>
 			<span>{fullName}</span>
 		</div>
