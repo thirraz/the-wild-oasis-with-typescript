6cdb67c565eddf2b5868e7acc525feca6894ed21
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-04-22 10:19:15 -0300

    update: data upload and active <Filter/> buttons

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -5,8 +5,8 @@ import { useLogin } from "./useLogin"
 import SpinnerMini from "../../ui/SpinnerMini"
 
 function LoginForm() {
-	const [email, setEmail] = useState("jonas@example.com")
-	const [password, setPassword] = useState("passpass")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
 	const { login, isPending } = useLogin()
 
 	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,18 @@ import Logo from "../ui/Logo"
 
 function Login() {
 	return (
-		<main className="min-h-[100dvh] flex flex-col items-center justify-center bg-grey-100 dark:bg-dark-grey-100 gap-5">
+		<main className="relative min-h-[100dvh] flex flex-col items-center justify-center bg-grey-100 dark:bg-dark-grey-100 gap-5">
+			<div className="text-sm px-6 py-5 max-w-64 border border-grey-600 dark:border-dark-grey-border-grey-600 absolute top-10 right-10 text-grey-600 dark:text-dark-grey-600 rounded-md shadow-sm bg-grey-0">
+				<h2 className="font-bold uppercase mb-4">
+					Use this e-mail and password to use this app. Thanks!
+				</h2>
+				<p>
+					<span className="font-semibold">e-mail:</span> jonas@example.com
+				</p>
+				<p>
+					<span className="font-semibold">password:</span> passpass
+				</p>
+			</div>
 			<Logo />
 			<h3 className="text-2xl uppercase font-bold mt-3">
 				Log in to your account
